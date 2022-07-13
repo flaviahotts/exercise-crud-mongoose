0ef2f9e9f1ac720b72ba8fa6350326f6c21137b3
Flavia <flavia.hotts@gmail.com>
2022-07-13 20:48:26 +0100

    crud-read done

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const app = express();
 
 app.use(express.json());
 
+const todoRouter = require("./routes/todo.router")
+app.use("/todo", todoRouter);
+
 app.listen(Number(process.env.PORT), () => {
 console.log(`Server up and running at port ${process.env.PORT}`);
 });
--- routes/Todo.router.js
+++ routes/Todo.router.js
@@ -1,3 +1,21 @@
 const router = require("express").Router();
 
 const TodoModel = require("../models/Todo.model");
+
+//CRUD
+//Create
+
+router.post("/create-todo", async (req, res) => {
+    try {
+    const newTodo = await TodoModel.create(req.body);
+
+    return res.status(201).json(newTodo);
+    } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+    }
+});
+
+
+
+module.exports = router;
